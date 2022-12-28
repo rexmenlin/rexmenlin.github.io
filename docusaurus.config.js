a29835a5131295387f37286dd7beda31d5aebe1b
Rex Lin <rexmenlin@yahoo.com.tw>
2022-12-28 15:11:36 +0800

    init2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rexmenlin', // Usually your GitHub org/user name.
   projectName: 'rexmenlin.github.io', // Usually your repo name.
-
+  trailingSlash: true,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
